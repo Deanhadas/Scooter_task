5802e52138fd5ae652739386ee70cb5dbbb4756c
Deanhadas <72769692+Deanhadas@users.noreply.github.com>
2023-05-17 03:05:02 +0300

    Update server.js
    del deploy

--- scooter-exercise/backend/server.js
+++ scooter-exercise/backend/server.js
@@ -15,8 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../build")))
-
 const port = process.env.PORT || 3000;
 
 //Connect to MongoDB
